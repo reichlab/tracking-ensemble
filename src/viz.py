49cd01b90590e1ada238b465fd55ed4be2e8789f
Abhinav Tushar <lepisma@fastmail.com>
2018-03-12 18:10:24 -0400

    mm plot code

--- src/viz.py
+++ src/viz.py
@@ -1,5 +1,62 @@
 """
-Source for generating the visualizations
+Source for generating visualizations
 """
 
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from typing import List, Tuple
+
+
+Vec = np.ndarray
+Color = Tuple[float, float, float, float]
+
+
+def find_theta(m: Vec, m_hat: Vec) -> float:
+    """
+    Return value of theta for given m and m_hat.
+    """
+
+    return np.max(np.abs(m - m_hat))
+
+
+def plot_mm(ax: plt.Axes, m: Vec, m_hat: Vec, color: Color):
+    """
+    Place mm plot on the given axes
+    """
+
+    lim = 1.2
+
+    # Draw feasible region
+    ax.set_xlim(0, lim)
+    ax.set_ylim(0, lim)
+    ax.fill([1,1,0], [0,1,1], "k", alpha=0.05)
+
+    # Draw theta lines
+    theta = find_theta(m, m_hat)
+    x_line = mpl.lines.Line2D([theta, lim], [0, lim - theta],
+                              linestyle="--", color=color, alpha=0.7,
+                              label=f"$\\theta = {theta:.2f}$")
+    y_line = mpl.lines.Line2D([0, lim - theta], [theta, lim], linestyle="--", color=color, alpha=0.7)
+    ax.add_line(x_line)
+    ax.add_line(y_line)
+
+    # Draw middle line
+    ax.add_line(mpl.lines.Line2D([0, lim], [0, lim], linestyle="--", color="gray", alpha=0.3))
+
+    # Draw scatter
+    sct = ax.scatter(m, m_hat, s=40, alpha=0.2, color=color)
+    ax.set_xlabel("$m$")
+    ax.set_ylabel("$\hat{m}$")
+
+    # Draw mean point
+    mp = (np.mean(m), np.mean(m_hat))
+    ax.add_patch(mpl.patches.Circle(mp, radius=0.015, color=color, alpha=0.8))
+
+    # Draw mean projections
+    mp_x = mpl.lines.Line2D([mp[0], mp[0]], [0, mp[1]], linestyle="dotted", color="gray", alpha=0.3)
+    ax.add_line(mp_x)
+    mp_y = mpl.lines.Line2D([0, mp[0]], [mp[1], mp[1]], linestyle="dotted", color="gray", alpha=0.3)
+    ax.add_line(mp_y)
+
+    ax.legend()
